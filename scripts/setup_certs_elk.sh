68ec2154fe6637b67aa1288822b1a8b5b0875c34
Pedro el banquero <60758685+pedroelbanquero@users.noreply.github.com>
2020-03-07 17:27:59 -0500

    Update setup_certs_elk.sh

--- scripts/setup_certs_elk.sh
+++ scripts/setup_certs_elk.sh
@@ -6,7 +6,7 @@ mkdir tmp
 mkdir tmp/cert_blog
 
 sudo echo "127.0.0.1 kibana.local logstash.local" >> /etc/hosts
-sudo echo "blackhole $1 blackhole" >> /etc/hosts
+sudo echo "127.0.0.1 $1" >> /etc/hosts
 
 sudo /usr/share/elasticsearch/bin/elasticsearch-certutil cert ca --pem --in ~/tmp/cert_blog/instance.yml --out ~/tmp/cert_blog/certs.zip
 
