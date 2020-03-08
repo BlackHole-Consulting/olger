df856fd937f0efda256d1e46e486b4fd2157beff
korea project <koreaproject@protonmail.com>
2020-03-07 21:37:44 -0600

    install elk base

--- playbooks/files/install_elk.sh
+++ playbooks/files/install_elk.sh
@@ -1,8 +1,11 @@
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-sudo apt-get update && sudo apt-get install elasticsearch
 
-sudo apt install kibana
+sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 
-sudo apt install filebeat
+sudo apt-get install apt-transport-https -y
+sudo echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+
+sudo apt-get update && sudo apt-get install elasticsearch -y
+
+sudo apt install kibana -y
+
+sudo apt install filebeat -y
