3602c0e5882840bf1941bc73d70ee0498cd65675
xuann <xuann@cryptolab.net>
2020-04-27 22:29:57 -0700

    depurado install requirement. entre otras cosas ansible se instala desde pip, etc. Parece que funciona mejor

---
+++ install-dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt-get install python-pip
+sudo apt-get install xdot
+sudo apt install nmap
+sudo apt install openjdk-8-jdk
+sudo apt install python3-libnmap
+sudo apt install python3-dateutil
+sudo apt install python3-xmltodict
+sudo apt install npm
+sudo apt install metasploit-framework
+sudo apt install sshpass
+npm init
+npm install npm
+npm install
+npm audit fix
+sudo npm install shodan -g
+curl 'https://bootstrap.pypa.io/get-pip.py' > get-pip.py && sudo python get-pip.py
+sudo pip install -r requirements.txt
+mkdir ./reports
+mkdir ./data
+mkdir ./lists
+mkdir -p ./web/graphs
